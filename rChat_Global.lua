64c3416e8520bb8a8ad57cf36197c1b6fa32811c
Dolores Scott <scottd@texas.net>
2021-05-30 19:01:56 -0500

    API bump. Blackwood

--- rChat_Global.lua
+++ rChat_Global.lua
@@ -8,7 +8,7 @@ local SF = LibSFUtils
 
 rChat = {
     name = "rChat",
-    version = "1.18",
+    version = "1.20",
     settingName = "rChat",
     settingDisplayName = "rChat",
     author = "Shadowfen",
