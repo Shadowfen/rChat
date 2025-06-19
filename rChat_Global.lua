9590c96b45cde3ef52ad9fa8c0c033880ccd6bba
Shadowfen <8352668+Shadowfen@users.noreply.github.com>
2025-06-18 23:26:40 -0500

    Convert from .txt manifest to .addon

--- rChat_Global.lua
+++ rChat_Global.lua
@@ -8,7 +8,7 @@ local SF = LibSFUtils
 
 rChat = {
     name = "rChat",
-    version = "1.44",
+    version = "1.45",
     settingName = "rChat",
     settingDisplayName = "rChat",
     author = "Shadowfen",
