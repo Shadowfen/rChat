0425b1ddc6cf03abb564dd1d174f68e765643430
Shadowfen <shadowfen@gmail.com>
2026-09-16 15:05:36 -0500

    Update to LibSFUtils 77 and use of new logDebug functions

--- rChat_Global.lua
+++ rChat_Global.lua
@@ -8,17 +8,25 @@ local SF = LibSFUtils
 
 rChat = {
     name = "rChat",
-    version = SF.colors.gold:Colorize("2.0.5"),
+    version = SF.colors.gold("2.0.5"),
     settingName = "rChat",
-    settingDisplayName = SF.colors.gold:Colorize("rChat"),
-    author =  SF.colors.purple:Colorize("Shadowfen"),
+    settingDisplayName = SF.colors.gold("rChat"),
+    author =  SF.colors.purple("Shadowfen"),
     savedvar = "RCHAT_OPTS",
     sv_version = 2,
     evtmgr = SF.EvtMgr:New("rChat"),
     hookmgr = SF.HookManager:New("rChat"),
 }
 
-rChat_Logger = SF.SafeLoggerFunction(rChat, "logger", "rChat")
+-- Create the delayed instantiation logger functor and utility functions for AutoCategory.
+rChat_Logger, rChat.logDebug, rChat.logWouldDebug = 
+            SF.InitSafeLogger(rChat, "logger", "rChat")
+
+--[[ The following SetDebug() call is commented out because it severely slows down 
+    addon operation. Turning it on does however provide lots and lots of debug logging.
+    Never leave this uncommented when releasing!!
+--]]
+--rChat_Logger():SetDebug(true)
 
 SF.LoadLanguage(rChat_localization_strings, "en")
 
@@ -26,30 +34,3 @@ rChat.data = {
     cachedMessages = {}, -- This must be init before OnAddonLoaded because it will receive data before this event.
 }
 
-rChat_Logger = SF.SafeLoggerFunction(rChat, "logger", "rChat")
-
---[[
-    The following SetDebug() call is commented out because it severely slows down 
-    addon operation. Turning it on does however provide lots and lots of debug logging.
-    Never leave this uncommented when releasing!!
---]]
---rChat_Logger():SetDebug(true)
-
--- convenience function for a call to rChat_Logger():Debug(SF.str(...))
--- only done for Debug() because there is no special handling for the other message levels
--- always returns nil
-function rChat.logDebug(...)
-    local n = select("#", ...)
-    if n == 0 then return end
-
-    local logger = rChat_Logger()
-    -- skip parameter processing if they are not going to be used.
-    if not logger.enabled or not logger.SFenableDebug then return end
-
-    if n == 1 then
-        logger:Debug(...)
-
-    else
-        logger:Debug(SF.str(...))
-   end
-end
